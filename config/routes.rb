6cebd93010c18016dffbfdad29dd847b7718b122
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-06-08 20:01:49 -0400

    added comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  # get route with a specific status -"Applied/New/Interviewed"
   get '/jobs/:status', to:'jobs#show'
+
+  # post route for creating a new job
   post '/jobs', to: 'jobs#create'
+
+  # update route to update status -"Applied/New/Interviewed"
   put '/jobs/:id', to: 'jobs#update'
+
+  # delete route to delete a job"
   delete '/jobs/:id', to: 'jobs#delete'
 
 end
