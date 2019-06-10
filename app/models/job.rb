0eebb0de5c8cf4447bd176da0f7e4aeab1c03aa4
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-10 10:51:53 -0400

    Added databaseurl to rails

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,9 @@
 class Job
-
-  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'jobtraxer-rails_development'})
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+    DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'jobtraxer-rails_development'})
 
   def self.find(status)
     results = DB.exec("SELECT * FROM jobs WHERE status LIKE '#{status}' ;")
